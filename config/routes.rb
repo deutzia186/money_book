e879f584fc0c7120b97648d97ec8a2c0d80b0402
deutzia186 <samurai_tu186@hotmail.com>
2020-03-30 16:06:31 +0900

    lists controller create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "books#index"
   resources :users, only: [:index, :edit, :show, :update]
-  resources :books, only: [:new, :create, :edit, :update] do
-  end
-end
+  resources :users, only: [:index, :edit, :update]
+  resources :lists, only: [:index]
+end 
