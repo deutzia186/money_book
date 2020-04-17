226d9d414815fc52fdae83174b25471e96d0d2c7
deutzia186 <samurai_tu186@hotmail.com>
2020-04-17 20:16:58 +0900

    category.name

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def index
-    @categories = Category.all # 複数あるので変数名も複数系
+    @categories = Category.all
     @book = current_user.books.order("date ASC") # userモデルにhas_many :booksとあるのでこう書ける
   end
 end
