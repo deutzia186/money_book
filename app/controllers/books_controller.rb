39b8994b1a25a01d155ddb01c7d399ca66db1169
deutzia186 <samurai_tu186@hotmail.com>
2020-04-18 21:36:34 +0900

    book_edit_page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,16 @@ class BooksController < ApplicationController
     redirect_to new_book_path
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to new_book_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:select, :date, :category_id, :price, :image, :image_cache, :memo,).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "books#index"
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :new, :create]
+  resources :books, only: [:index, :new, :create, :edit, :update]
   resources :lists, only: [:index]
 end 
