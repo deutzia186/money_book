5ecbdceaadc5bcf318ae32914042fe8a825104d1
deutzia186 <samurai_tu186@hotmail.com>
2020-04-30 20:36:00 +0900

    ransack

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,19 @@
 class ListsController < ApplicationController
-  before_action :set_ransack
-
   def index
     @categories = Category.all
-    @book = current_user.books.order("date ASC").page(params[:page]).per(5) # userモデルにhas_many :booksとあるのでこう書ける
+    @books = current_user.books.order("date ASC").page(params[:page]).per(5) # userモデルにhas_many :booksとあるのでこう書ける
+
+    @q = Book.ransack(params[:q])
+    @searches = @q.result(distinct: true)
+  end
+
+  def search
+    @q = Book.ransack(search_params)
+    @searches = @q.result(distinct: true)
   end
 
   private
-  def set_ransack
-    @q = Book.ransack(params[:q])
-    @results = @q.result(distinct: true)
+  def search_params
+    params.require(:q).permit(:date_gteq, :date_lteq, :memo_cont, :select_eq, :category_id_eq, :price_gteq, :price_lteq)
   end
 end
