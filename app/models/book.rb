2a7682effdab8dd8d66f984c51c8ac90972633f0
deutzia186 <samurai_tu186@hotmail.com>
2020-05-19 18:20:05 +0900

    chartkick

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_many :bookmarks
-  
+  has_many   :bookmarks
+  belongs_to :category
+
   def bookmark_by?(user)
     bookmarks.where(user_id: user.id).exists?
   end
 
   mount_uploader :image, ImageUploader
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
 end
