ad9e53a048f4d35d2c07abc326f59a9055ddc3e3
deutzia186 <samurai_tu186@hotmail.com>
2020-06-01 21:23:22 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,6 +63,5 @@
 
 server '54.178.112.22', user: 'ec2-user', roles: %w{app db web}
 
-config.assets.css_compressor = :scss
-config.assets.enabled = true
-config.assets.compile = true
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
