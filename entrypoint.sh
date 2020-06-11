04f48c1c08357ce7b9ebff63f853939fd0917928
deutzia186 <samurai_tu186@hotmail.com>
2020-06-11 19:14:53 +0900

    docker mysql update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
