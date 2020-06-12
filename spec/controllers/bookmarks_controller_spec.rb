69503deaa045235e22620ee1f823cf8e0af73735
deutzia186 <samurai_tu186@hotmail.com>
2020-06-12 21:56:08 +0900

    update

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BookmarksController, type: :controller do
 
-  describe "GET #create" do
+  # describe "GET #create" do
   #   it "returns http success" do
   #     get :create
   #     expect(response).to have_http_status(:success)
