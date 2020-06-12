266a81122feef1135c888dd78cd0aac0e9530d0b
deutzia186 <samurai_tu186@hotmail.com>
2020-06-12 16:19:27 +0900

    Rspec install

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it "nameがない場合は登録できないこと" do
+      user = User.new(name: "", email: "kkk@gmail.com", password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+  end
 end
