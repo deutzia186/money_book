996b12df0997994372b8992e81bf2b0d4c658fff
deutzia186 <samurai_tu186@hotmail.com>
2020-06-29 21:32:04 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
-
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
