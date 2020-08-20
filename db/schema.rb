c1da626e72891a0d9c3188480dcb9b2ec48aff9a
deutzia186 <samurai_tu186@hotmail.com>
2020-08-20 15:11:51 +0900

    docker test update

--- db/schema.rb
+++ db/schema.rb
@@ -51,9 +51,9 @@ ActiveRecord::Schema.define(version: 2020_05_17_055901) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["name"], name: "index_users_on_name"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "bookmarks", "books"
